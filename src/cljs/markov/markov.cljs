e0594f88559ab5a89138c654008afd52dcd2fd1c
Diego Basch <dbasch@gmail.com>
2013-12-05 00:20:52 -0800

    simplified a bit

--- src/cljs/markov/markov.cljs
+++ src/cljs/markov/markov.cljs
@@ -13,28 +13,23 @@
 
 (defn markov-data [line]
   (let [l (if (= \. (last line)) line (str line "."))
-        words (filter #(re-find #"[a-z]" %) (clojure.string/split l #"\s"))
+        words (clojure.string/split l #"\s")
         ks (concat ["*START*"] (butlast words))
         pairs (partition 2 (interleave ks words))
         maps (for [p pairs] {(first p) [(second p)]})]
     (apply merge-with concat maps)))
 
-(defn random-word [ws]
-  (let [n (count ws)
-        i (rand-int n)]
-    (nth ws i)))
-
-(defn markov [text]
+(defn text-to-markov [text]
   (let [sentences (clojure.string/split text #"\n")]
     (apply merge-with concat (map markov-data sentences))))
 
 (defn generate []
-  (let [data (markov (input))]
-    (loop [k (random-word (data "*START*"))
-           acc [k]]
-      (let [ws (data k)
-            w (random-word ws)
+  (let [data (text-to-markov (input))]
+    (loop [ws (data "*START*")
+           acc []]
+      (let [w (-> ws shuffle first)
+            nws (data w)
             nacc (concat acc [w])]
         (if (= \. (last w))
           (output (clojure.string/join " " nacc))
-          (recur w nacc))))))
+          (recur nws nacc))))))
