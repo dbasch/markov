f93bdb16349358e34a98a7a64f6799f93eedf257
Diego Basch <dbasch@gmail.com>
2013-12-12 14:16:33 -0800

    simplified

--- src/cljs/markov/markov.cljs
+++ src/cljs/markov/markov.cljs
@@ -11,18 +11,20 @@
   
 ;; Markov chain generation
 
-(defn markov-data [line]
-  (let [l (if (= \. (last line)) line (str line "."))
-        words (cons "*START*" (clojure.string/split l #"\s"))
-        maps (for [p (partition 2 1 words)] {(first p) [(second p)]})]
+(defn markov-data [text]
+  (let [maps
+        (for [line (clojure.string/split text #"\.")
+              m (let [l (str line ".")
+                      words
+                      (cons "*START*" (clojure.string/split l #"\s+"))]
+                  (for [p (partition 2 1 (remove empty? words))]
+                    {(first p) [(second p)]}))]
+          m)]
     (apply merge-with concat maps)))
 
-(defn text-to-markov [text]
-  (let [sentences (clojure.string/split text #"\n")]
-    (apply merge-with concat (map markov-data sentences))))
 
 (defn generate []
-  (let [data (text-to-markov (input))]
+  (let [data (markov-data (input))]
     (loop [ws (data "*START*")
            acc []]
       (let [w (rand-nth ws)
